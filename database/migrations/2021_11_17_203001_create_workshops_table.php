505a4cf528b624dece02d384bd45a2b7c6508b91
Mahfuj-Shohug <mahfuj.shohug@gmail.com>
2021-11-18 02:36:14 +0600

    DS Summit 2021 Page Updated

---
+++ database/migrations/2021_11_17_203001_create_workshops_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateWorkshopsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('workshops', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('varsity');
+            $table->string('dept');
+            $table->string('wname');
+            $table->string('phone');
+            $table->string('gmail');
+            $table->string('trid');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('workshops');
+    }
+}
